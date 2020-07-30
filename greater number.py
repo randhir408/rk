e173764277c45df81f514abd3155d5541b500f64
randhir408 <randhirkumarsonukumar@gmail.com>
2020-07-30 18:32:01 +0530

    my file

---
+++ greater number.py
@@ -0,0 +1,13 @@
+num1=int(input("enter the  first number="))
+num2=int(input("enter the second number="))
+num3=int(input("enter the third nunber="))
+if num1>num2:
+    if num1>num3:
+      print(num1,"is greater")
+    else:
+      print(num3,"is greater")
+else:
+    if num1>num3:
+      print(num1,"is greater")
+    else:
+      print(num3,"is greater")
